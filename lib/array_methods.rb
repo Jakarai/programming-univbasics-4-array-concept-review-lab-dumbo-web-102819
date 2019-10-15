5e246617e5ceab52c8a1c1a634a0f4e158ecc423
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-15 19:02:35 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,7 @@ def find_element_index(array, value_to_find)
     array.include?(value_to_find) do 
     puts array.index(value_to_find)
   end
-    while true do
+    while true do |array.index|
       puts array.index
 end
 
